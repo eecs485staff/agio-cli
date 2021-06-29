78e3c2c1e94a55dae2aa9da23b211e2f47a71e9c
Andrew DeOrio <awdeorio@umich.edu>
2021-06-29 12:06:58 -0400

    projects --list test works

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,10 +17,11 @@ def api_requests_mock(requests_mock):
             "first_name": "Andrew",
             "last_name": "DeOrio",
             "email": "",
-            "is_superuser": false
+            "is_superuser": False
         }
         """)
     )
+
     # Course list
     requests_mock.get(
         "https://autograder.io/api/users/5/courses_is_admin_for/",
@@ -57,6 +58,7 @@ def api_requests_mock(requests_mock):
             }
         ])
     )
+
     # Course detail
     requests_mock.get(
         "https://autograder.io/api/courses/109/",
@@ -70,4 +72,286 @@ def api_requests_mock(requests_mock):
             "allowed_guest_domain": "@umich.edu",
             "last_modified": "2021-04-07T02:19:22.818992Z"
         })
+    )
+
+    # Project list
+    requests_mock.get(
+        "https://autograder.io/api/courses/109/projects/",
+        text=json.dumps([
+            {
+                "pk": 1007,
+                "name": "Project 5 - Search Engine",
+                "last_modified": "2021-06-21T16:42:49.476960Z",
+                "course": 109,
+                "visible_to_students": True,
+                "closing_time": "2021-06-21T04:30:00Z",
+                "soft_closing_time": "2021-06-21T03:59:00Z",
+                "disallow_student_submissions": False,
+                "disallow_group_registration": False,
+                "guests_can_submit": False,
+                "min_group_size": 1,
+                "max_group_size": 3,
+                "submission_limit_per_day": 3,
+                "allow_submissions_past_limit": False,
+                "groups_combine_daily_submissions": False,
+                "submission_limit_reset_time": "00:00:00",
+                "submission_limit_reset_timezone": "US/Eastern",
+                "num_bonus_submissions": 0,
+                "total_submission_limit": None,
+                "allow_late_days": False,
+                "ultimate_submission_policy": "best",
+                "hide_ultimate_submission_fdbk": False,
+                "instructor_files": [
+                    {
+                        "pk": 24749,
+                        "project": 1007,
+                        "name": "autograder-4d215e29.tar.xz",
+                        "last_modified": "2021-06-14T03:39:17.838619Z",
+                        "size": 4306452
+                    },
+                    {
+                        "pk": 24750,
+                        "project": 1007,
+                        "name": "install",
+                        "last_modified": "2021-06-14T03:39:18.125266Z",
+                        "size": 2906
+                    }
+                ],
+                "expected_student_files": [
+                    {
+                        "pk": 2547,
+                        "project": 1007,
+                        "pattern": "submit.tar.xz",
+                        "min_num_matches": 1,
+                        "max_num_matches": 1,
+                        "last_modified": "2021-04-07T00:31:28.029083Z"
+                    }
+                ],
+                "has_handgrading_rubric": False,
+                "send_email_on_submission_received": True,
+                "send_email_on_non_deferred_tests_finished": True,
+                "use_honor_pledge": True,
+                "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+            },
+            {
+                "pk": 1008,
+                "name": "Project 3 - Client-side Dynamic Pages",
+                "last_modified": "2021-05-27T22:01:15.359868Z",
+                "course": 109,
+                "visible_to_students": True,
+                "closing_time": "2021-05-26T04:30:00Z",
+                "soft_closing_time": "2021-05-26T03:59:00Z",
+                "disallow_student_submissions": False,
+                "disallow_group_registration": False,
+                "guests_can_submit": False,
+                "min_group_size": 1,
+                "max_group_size": 3,
+                "submission_limit_per_day": 3,
+                "allow_submissions_past_limit": False,
+                "groups_combine_daily_submissions": False,
+                "submission_limit_reset_time": "00:00:00",
+                "submission_limit_reset_timezone": "US/Eastern",
+                "num_bonus_submissions": 0,
+                "total_submission_limit": None,
+                "allow_late_days": False,
+                "ultimate_submission_policy": "best",
+                "hide_ultimate_submission_fdbk": False,
+                "instructor_files": [
+                    {
+                        "pk": 22508,
+                        "project": 1008,
+                        "name": "autograder-bcc78f56-dirty.tar.gz",
+                        "last_modified": "2021-05-18T22:00:36.641145Z",
+                        "size": 168836
+                    },
+                    {
+                        "pk": 21949,
+                        "project": 1008,
+                        "name": "install",
+                        "last_modified": "2021-05-18T22:00:36.805560Z",
+                        "size": 3057
+                    }
+                ],
+                "expected_student_files": [
+                    {
+                        "pk": 2548,
+                        "project": 1008,
+                        "pattern": "submit.tar.gz",
+                        "min_num_matches": 1,
+                        "max_num_matches": 1,
+                        "last_modified": "2021-04-07T00:31:30.640369Z"
+                    }
+                ],
+                "has_handgrading_rubric": False,
+                "send_email_on_submission_received": True,
+                "send_email_on_non_deferred_tests_finished": True,
+                "use_honor_pledge": True,
+                "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+            },
+            {
+                "pk": 1005,
+                "name": "Project 1 - Templated Static Site Generator",
+                "last_modified": "2021-05-13T19:46:38.254102Z",
+                "course": 109,
+                "visible_to_students": True,
+                "closing_time": "2021-05-12T04:30:00Z",
+                "soft_closing_time": "2021-05-12T03:59:00Z",
+                "disallow_student_submissions": False,
+                "disallow_group_registration": False,
+                "guests_can_submit": True,
+                "min_group_size": 1,
+                "max_group_size": 1,
+                "submission_limit_per_day": 3,
+                "allow_submissions_past_limit": False,
+                "groups_combine_daily_submissions": False,
+                "submission_limit_reset_time": "00:00:00",
+                "submission_limit_reset_timezone": "US/Eastern",
+                "num_bonus_submissions": 0,
+                "total_submission_limit": None,
+                "allow_late_days": False,
+                "ultimate_submission_policy": "best",
+                "hide_ultimate_submission_fdbk": False,
+                "instructor_files": [
+                    {
+                        "pk": 21908,
+                        "project": 1005,
+                        "name": "autograder-6f82202d.tar.gz",
+                        "last_modified": "2021-04-07T02:16:02.992523Z",
+                        "size": 11323
+                    },
+                    {
+                        "pk": 21909,
+                        "project": 1005,
+                        "name": "install",
+                        "last_modified": "2021-04-07T02:16:03.147228Z",
+                        "size": 2230
+                    }
+                ],
+                "expected_student_files": [
+                    {
+                        "pk": 2545,
+                        "project": 1005,
+                        "pattern": "submit.tar.gz",
+                        "min_num_matches": 1,
+                        "max_num_matches": 1,
+                        "last_modified": "2021-04-07T00:31:21.379258Z"
+                    }
+                ],
+                "has_handgrading_rubric": False,
+                "send_email_on_submission_received": True,
+                "send_email_on_non_deferred_tests_finished": True,
+                "use_honor_pledge": True,
+                "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+            },
+            {
+                "pk": 1009,
+                "name": "Project 2 - Server-side Dynamic Pages",
+                "last_modified": "2021-05-19T13:03:53.221672Z",
+                "course": 109,
+                "visible_to_students": True,
+                "closing_time": "2021-05-19T04:30:00Z",
+                "soft_closing_time": "2021-05-19T03:59:00Z",
+                "disallow_student_submissions": False,
+                "disallow_group_registration": False,
+                "guests_can_submit": False,
+                "min_group_size": 1,
+                "max_group_size": 3,
+                "submission_limit_per_day": 3,
+                "allow_submissions_past_limit": False,
+                "groups_combine_daily_submissions": False,
+                "submission_limit_reset_time": "00:00:00",
+                "submission_limit_reset_timezone": "US/Eastern",
+                "num_bonus_submissions": 0,
+                "total_submission_limit": None,
+                "allow_late_days": False,
+                "ultimate_submission_policy": "best",
+                "hide_ultimate_submission_fdbk": False,
+                "instructor_files": [
+                    {
+                        "pk": 22506,
+                        "project": 1009,
+                        "name": "autograder-788e5076.tar.gz",
+                        "last_modified": "2021-05-11T19:21:55.373807Z",
+                        "size": 282326
+                    },
+                    {
+                        "pk": 22507,
+                        "project": 1009,
+                        "name": "install",
+                        "last_modified": "2021-05-11T19:21:55.596257Z",
+                        "size": 2095
+                    }
+                ],
+                "expected_student_files": [
+                    {
+                        "pk": 2549,
+                        "project": 1009,
+                        "pattern": "submit.tar.gz",
+                        "min_num_matches": 1,
+                        "max_num_matches": 1,
+                        "last_modified": "2021-04-07T00:31:33.584510Z"
+                    }
+                ],
+                "has_handgrading_rubric": False,
+                "send_email_on_submission_received": True,
+                "send_email_on_non_deferred_tests_finished": True,
+                "use_honor_pledge": True,
+                "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+            },
+            {
+                "pk": 1006,
+                "name": "Project 4 - MapReduce",
+                "last_modified": "2021-06-15T03:59:11.942724Z",
+                "course": 109,
+                "visible_to_students": True,
+                "closing_time": "2021-06-14T04:30:00Z",
+                "soft_closing_time": "2021-06-14T03:59:00Z",
+                "disallow_student_submissions": False,
+                "disallow_group_registration": False,
+                "guests_can_submit": False,
+                "min_group_size": 1,
+                "max_group_size": 3,
+                "submission_limit_per_day": 3,
+                "allow_submissions_past_limit": False,
+                "groups_combine_daily_submissions": False,
+                "submission_limit_reset_time": "00:00:00",
+                "submission_limit_reset_timezone": "US/Eastern",
+                "num_bonus_submissions": 0,
+                "total_submission_limit": None,
+                "allow_late_days": False,
+                "ultimate_submission_policy": "best",
+                "hide_ultimate_submission_fdbk": False,
+                "instructor_files": [
+                    {
+                        "pk": 21900,
+                        "project": 1006,
+                        "name": "autograder-8b2ec48d.tar.gz",
+                        "last_modified": "2021-04-07T00:31:25.318107Z",
+                        "size": 3077619
+                    },
+                    {
+                        "pk": 21901,
+                        "project": 1006,
+                        "name": "install",
+                        "last_modified": "2021-04-07T00:31:25.441090Z",
+                        "size": 2098
+                    }
+                ],
+                "expected_student_files": [
+                    {
+                        "pk": 2546,
+                        "project": 1006,
+                        "pattern": "submit.tar.gz",
+                        "min_num_matches": 1,
+                        "max_num_matches": 1,
+                        "last_modified": "2021-04-07T00:31:25.503082Z"
+                    }
+                ],
+                "has_handgrading_rubric": False,
+                "send_email_on_submission_received": True,
+                "send_email_on_non_deferred_tests_finished": True,
+                "use_honor_pledge": True,
+                "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+            }
+        ])
     )
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -3,11 +3,9 @@
 These tests use the Click testing interface.
 https://click.palletsprojects.com/en/8.0.x/testing/
 """
-import json
 import textwrap
 import click
 import click.testing
-import pytest
 from agiocli.__main__ import main
 
 
@@ -15,24 +13,28 @@ from agiocli.__main__ import main
 # pylint: disable=unused-argument
 
 
-@pytest.fixture(name="api_mock")
-def api_requests_mock(requests_mock):
-    """Mock Autograder API with hardcoded responses."""
-    assert False, "IMPLEMENT ME"
-
-
-def test_projects_list(api_mock):
+def test_projects_list_course_pk(api_mock):
     """Verify agio projects list option.
 
-    $ agio projects --list
+    $ agio projects --list --course 109
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
     runner = click.testing.CliRunner()
-    result = runner.invoke(main, ["projects", "--list"])
+    result = runner.invoke(main, [
+        "projects",
+        "--list",
+        "--course", "109",
+    ])
     assert result.exit_code == 0
     assert result.output == textwrap.dedent("""\
-        [129]\tEECS 485 Fall 2021
-        [109]\tEECS 485 Spring 2021
-        [111]\tEECS 280 Spring 2021
+        [1005]	Project 1 - Templated Static Site Generator
+        [1009]	Project 2 - Server-side Dynamic Pages
+        [1008]	Project 3 - Client-side Dynamic Pages
+        [1006]	Project 4 - MapReduce
+        [1007]	Project 5 - Search Engine
     """)
 
 
@@ -40,8 +42,12 @@ def test_projects_empty(api_mock, mocker):
     """Verify projects subcommand no input.
 
     $ agio projects
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
-    # Mock user-selection menu, users selects course pk 109
+    # Mock user-selection menu, users selects course 109, then project 1005
     course_109 = {
         'pk': 109,
         'name': 'EECS 485',
@@ -52,9 +58,65 @@ def test_projects_empty(api_mock, mocker):
         'allowed_guest_domain': '@umich.edu',
         'last_modified': '2021-04-07T02:19:22.818992Z'
     }
-    mocker.patch("pick.pick", return_value=(project_109, 1))
-
-    # FIXME: Mock user-selection menu, users selects project pk 1008
+    project_1005 = {
+        "pk": 1005,
+        "name": "Project 1 - Templated Static Site Generator",
+        "last_modified": "2021-05-13T19:46:38.254102Z",
+        "course": 109,
+        "visible_to_students": True,
+        "closing_time": "2021-05-12T04:30:00Z",
+        "soft_closing_time": "2021-05-12T03:59:00Z",
+        "disallow_student_submissions": False,
+        "disallow_group_registration": False,
+        "guests_can_submit": True,
+        "min_group_size": 1,
+        "max_group_size": 1,
+        "submission_limit_per_day": 3,
+        "allow_submissions_past_limit": False,
+        "groups_combine_daily_submissions": False,
+        "submission_limit_reset_time": "00:00:00",
+        "submission_limit_reset_timezone": "US/Eastern",
+        "num_bonus_submissions": 0,
+        "total_submission_limit": None,
+        "allow_late_days": False,
+        "ultimate_submission_policy": "best",
+        "hide_ultimate_submission_fdbk": False,
+        "instructor_files": [
+            {
+                "pk": 21908,
+                "project": 1005,
+                "name": "autograder-6f82202d.tar.gz",
+                "last_modified": "2021-04-07T02:16:02.992523Z",
+                "size": 11323
+            },
+            {
+                "pk": 21909,
+                "project": 1005,
+                "name": "install",
+                "last_modified": "2021-04-07T02:16:03.147228Z",
+                "size": 2230
+            }
+        ],
+        "expected_student_files": [
+            {
+                "pk": 2545,
+                "project": 1005,
+                "pattern": "submit.tar.gz",
+                "min_num_matches": 1,
+                "max_num_matches": 1,
+                "last_modified": "2021-04-07T00:31:21.379258Z"
+            }
+        ],
+        "has_handgrading_rubric": False,
+        "send_email_on_submission_received": True,
+        "send_email_on_non_deferred_tests_finished": True,
+        "use_honor_pledge": True,
+        "honor_pledge_text": "I have neither given nor received aid on this project, nor have I concealed any violations of the Honor Code."
+    }
+    mocker.patch("pick.pick", return_value=[
+        (course_109, 1),
+        (project_1005, 1),
+    ])
 
     # Run agio and check output
     runner = click.testing.CliRunner()
@@ -69,6 +131,10 @@ def test_projects_pk(api_mock):
     """Verify projects subcommand with primary key input.
 
     $ agio projects 1008
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
     runner = click.testing.CliRunner()
     result = runner.invoke(main, ["projects", "1008"])
@@ -82,6 +148,10 @@ def test_projects_name(api_mock):
     """Verify projects subcommand with project name input.
 
     $ agio projects -c eecs485sp21 "Project 3 - Client-side Dynamic Pages"
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
     runner = click.testing.CliRunner()
     result = runner.invoke(main, [
@@ -99,6 +169,10 @@ def test_projects_shortcut(api_mock):
     """Verify projects subcommand with shortcut input.
 
     $ agio projects -c eecs485sp21 p3
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
     runner = click.testing.CliRunner()
     result = runner.invoke(main, [
@@ -116,6 +190,10 @@ def test_projects_no_course(api_mock):
     """Verify projects subcommand with no --course specified.
 
     $ agio projects p3
+
+    api_mock is a shared test fixture that mocks responses to REST API
+    requests.  It is implemented in conftest.py.
+
     """
     # FIXME mock course selection picker
     runner = click.testing.CliRunner()
